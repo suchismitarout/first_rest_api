872751ddc1b035696a5106d8bb39473e4fe2ce9f
suchismitarout <suchismitarout47@gmail.com>
2019-10-15 13:03:24 +0530

    updated

--- common/fileutils.py
+++ common/fileutils.py
@@ -1,4 +1,8 @@
 import json
+import os
+import logging
+logger = logging.getLogger(__name__)
+
 
 def read_new_line_json(path):
     with open(path, "r") as fr:
@@ -6,13 +10,29 @@ def read_new_line_json(path):
         p = content2.split("\n")
         return [json.loads(i) for i in p if len(i) > 0]
 
+
 def write_list_as_new_line_json(list_obj, path):
+    if list_obj == [{}]:
+        raise Exception("empty dictionary can not be inserted")
     with open(path, "w") as fw:
         for i in list_obj:
             fw.write(json.dumps(i))
             fw.write("\n")
-            
+
+
 def read_json_file(path):
+    if not os.path.exists(path):
+        try:
+            raise FileNotFoundError("File not found")
+        except FileNotFoundError as exp:
+            print("Error", exp)
+
+    if not path.endswith(".json"):
+        try:
+            raise ValueError("file extension is not json")
+        except ValueError as exp:
+            print("Error", exp)
+
     with open(path, "r") as fj:
         f = fj.read()
         data = json.loads(f)
