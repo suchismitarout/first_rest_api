35deff0ff139404e8a3b455fad87d83a8d16b857
suchismitarout <suchismitarout47@gmail.com>
2019-10-15 13:02:49 +0530

    updated

--- unit_tests/common/test_fileutils.py
+++ unit_tests/common/test_fileutils.py
@@ -1,6 +1,7 @@
 import unittest
-from common import fileutils
+from first_restapi.common import fileutils
 from os.path import dirname,abspath
+
 
 
 class Testreadfile(unittest.TestCase):
@@ -13,7 +14,7 @@ class Testreadfile(unittest.TestCase):
         dir_path = dirname(dirname(abspath(__file__)))
         print(dir_path)
         # file_path = dir_path + "/resources/" + file_name
-        file_path ="{}/resources/{}".format(dir_path,file_name)
+        file_path ="{}\\resources\\{}".format(dir_path,file_name)
         print(file_path)
         actual_output = fileutils.read_new_line_json(file_path)
         print(actual_output)
@@ -23,7 +24,7 @@ class Testreadfile(unittest.TestCase):
     def test_write_list_as_new_line_json(self):
         file_name = "data1.json"
         dir_path1 = dirname(dirname(abspath(__file__)))
-        file_path1 = "{}/resources/{}".format(dir_path1,file_name)
+        file_path1 = "{}\\resources\\{}".format(dir_path1,file_name)
         input_list = [{"name":"deepak"},{"name":"abc"}]
         fileutils.write_list_as_new_line_json(input_list,file_path1)
 
@@ -35,6 +36,29 @@ class Testreadfile(unittest.TestCase):
     def test_write_list_as_new_line_json_with_empty_dict(self):
         file_name = "data1.json"
         dir_path1 = dirname(dirname(abspath(__file__)))
-        file_path1 = "{}/resources/{}".format(dir_path1, file_name)
-        input_list = [{}, {}]
-        fileutils.write_list_as_new_line_json(input_list, file_path1)
+        file_path1 = "{}\\resources\\{}".format(dir_path1, file_name)
+        input_list = [{}]
+        self.assertRaises(Exception,fileutils.write_list_as_new_line_json,input_list, file_path1)
+
+    def test_read_json_file_with_nonexisting_file(self):
+        file_name = "pk.txt"
+        parent_path = dirname(abspath(__file__))
+        path = parent_path + "\\" + file_name
+        self.assertRaises(Exception,fileutils.read_json_file, path)
+
+    def test_read_json_file_without_json_extension(self):
+        file_name = "hn.py"
+        parent_path = dirname(abspath(__file__))
+        path = "{}\\{}".format(parent_path,file_name)
+        self.assertRaises(Exception,fileutils.read_json_file, path)
+
+    def test_read_json_file_with_json_extension(self):
+        file_name = "jk.json"
+        parent_path = dirname(abspath(__file__))
+        path = "{}\\{}".format(parent_path,file_name)
+        expected = fileutils.read_json_file(path)
+        actual = {"name": "abc"}
+        self.assertEqual(expected,actual)
+
+
+
