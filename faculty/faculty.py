82090b4ab3468487aa33bc5e1645dbeae3822753
suchismitarout <suchismitarout47@gmail.com>
2019-10-16 12:44:56 +0530

    updated

--- faculty/faculty.py
+++ faculty/faculty.py
@@ -1,7 +1,7 @@
 from flask import jsonify,request,json
 from flask import Blueprint
 from os.path import dirname,abspath
-from common import fileutils
+from first_restapi.common import fileutils,dbutils,validator
 
 # print(__name__)
 faculty_var = Blueprint('faculty', __name__, url_prefix='/faculty')
@@ -13,6 +13,7 @@ parent_dir_path = dirname(dirname(abspath(__file__)))
 res2 = fileutils.read_new_line_json(parent_dir_path + "/resources/" + file_name)
 # print(res2)
 
+
 @faculty_var.route('/getfaculty', methods=['GET'])
 def get_faculty_info():
     # print("ENTRY: get_faculty_info")
@@ -20,7 +21,7 @@ def get_faculty_info():
 
 
 @faculty_var.route('/getfacultybyname/<string:name>', methods=['GET'])
-def get_faculty_by_id(name):
+def get_faculty_by_name(name):
     # print("ENTRY: get_faculty_by_id name: {}".format(name))
     data = None
     for i in res2:
@@ -41,20 +42,15 @@ def delete_faculty_by_name(name):
 @faculty_var.route('/addnewfaculty', methods=['POST'])
 def add_new_faculty():
     #print("ENTRY: add_new_faculty")
-    name = request.form.get("name")
-    age = request.form.get("age")
-    salary = request.form.get("salary")
-    loc = request.form.get("loc")
-    f = {"name": name, "age": age, "salary": salary, "loc": loc}
-    data1 = request.get_json(f)
+    data1 = request.get_json()
     res2.append(data1)
+    validator.validate_id(data1["id"])
+    validator.validate_age(data1["age"])
+    conn = dbutils.connect_to_faculty_db("db_config.json")
+    dbutils.insert_data_to_faculty_database(conn, data1)
     file_name = "faculty_data.json"
     parent_dir_path = dirname(dirname(abspath(__file__)))
     fileutils.write_list_as_new_line_json(res2, parent_dir_path + "/resources/" + file_name)
-    # with open("/home/ninky/PycharmProjects/first_Rest_Api/faculty/faculty_data.json", "w") as fw1:
-    #     for j in res2:
-    #         fw1.write(json.dumps(j))
-    #         fw1.write("\n")
     return jsonify(res2)
 
 
